e5683f6de50dc84f67b13216aeaedfe76b10d864
Judy Tsai <sonic8776@gmail.com>
2024-07-14 17:25:25 +0800

    Modify UserRemoteDTO

--- Houzz-Pagination/Entity/Remote/UserRemoteRepository.swift
+++ Houzz-Pagination/Entity/Remote/UserRemoteRepository.swift
@@ -9,9 +9,11 @@ import Foundation
 
 struct UserRemoteDTO: Codable {
     
+    let id: String
     let lastName: String
     
     enum CodingKeys: String, CodingKey {
+        case id = "id"
         case lastName = "lastName"
     }
 }
