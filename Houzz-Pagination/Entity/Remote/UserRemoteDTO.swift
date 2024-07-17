4f26d21dd9ea067b1c6f3003c7e464e34cc0c9d6
Judy Tsai <sonic8776@gmail.com>
2024-07-17 18:23:19 +0800

    Add fields to UserRemoteDTO

--- Houzz-Pagination/Entity/Remote/UserRemoteDTO.swift
+++ Houzz-Pagination/Entity/Remote/UserRemoteDTO.swift
@@ -10,10 +10,14 @@ import Foundation
 struct UserRemoteDTO: Codable {
     
     let id: String
+    let firstName: String
     let lastName: String
+    let avatar: String
     
     enum CodingKeys: String, CodingKey {
         case id = "id"
+        case firstName = "firstName"
         case lastName = "lastName"
+        case avatar = "avatar"
     }
 }
